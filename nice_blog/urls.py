83a28d442ef6f0d6e67fcab394d24c319bfb7261
mdAshrafuddin <mdtanjil01753511918@gmail.com>
2021-01-23 15:47:44 +0600

    Nice Blog

---
+++ nice_blog/urls.py
@@ -0,0 +1,35 @@
+"""nice_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from posts.views import index, about, service, post, contact
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'), 
+    path('about/', about, name='about'),
+    path('service/', service, name='service'),
+    path('posts/', post, name='posts'),
+    path('contact/', contact, name='contact'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def service(request):
+    return render(request, 'service.html', {})
+
+def post(request):
+    return render(request, 'blog.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
